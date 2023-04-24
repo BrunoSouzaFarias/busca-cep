2f445c47392f266f4f7c13a996d5fcc18fb3f082
BrunoSouzaFarias <d2fake@gmail.com>
2023-04-24 16:13:11 -0300

    Tratamento de Erro com sweetalert2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const preEl = document.querySelector('pre');
@@ -10,10 +12,12 @@ async function handleClick() {
   try {
     const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     const data = await result.json();
+    
+    if (data.erro) throw new Error('Erro de cep inválido');
 
     preEl.innerHTML = JSON.stringify(data);
     return data;
   } catch (error) {
-    return error.message;
+    Swal.fire('Ops', error.message, 'error');
   }
 }
